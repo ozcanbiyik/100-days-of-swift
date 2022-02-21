4fa6a567f3bf3734c75abc8ccf490ec6c3764302
Özcan BIYIK <ozcanbiyik@outlook.com>
2022-02-21 04:35:45 +0300

    February2122-1
    variables, string interpolation.

---
+++ February2122.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Özcan Bıyık"
+name = "Can Biyik"
+
+// ismi let key değeri ile tanımlasaydık eğer yukarıdaki gibi değişkene yeni bir değer atayamazdık.
+
+// Tanım yaparken değişken tipini belli ediyorsak değişken adından sonra boşluk bırakmadan tanım yapmak önemli.
+
+var kullaniciAdi : String // Yanlış
+var kullaniciSoyadi: String // Doğru
+
+var yas: Int
+yas = 25
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude : Float
+longitude = -86.783333
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+var userName = "Ozcan"
+var userSurname = "Bıyık"
+var fullName = userName + " " + userSurname
+
+c > 3
+c < 3
+c >= 3
+c <= 3
+
+fullName == "Ozcan Bıyık"
+
+"Your age is \(yas) years old. In another \(yas) years will be \(yas * 2)."
+
